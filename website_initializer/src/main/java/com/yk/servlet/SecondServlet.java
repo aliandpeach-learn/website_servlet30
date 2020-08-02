6c7cd66220958adffe16b7cda3e8956340dc626f
aliandpeach <aliandpeach@gmail.com>
2020-08-02 19:56:52 +0800

    commit

--- website_initializer/src/main/java/com/yk/servlet/SecondServlet.java
+++ website_initializer/src/main/java/com/yk/servlet/SecondServlet.java
@@ -1,11 +1,15 @@
 package com.yk.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//@WebServlet
+//@WebFilter
 public class SecondServlet extends HttpServlet {
 
     private static final long serialVersionUID = 168843534054504354L;
--- website_initializer/src/main/java/com/yk/servlet/ServletInitializer.java
+++ website_initializer/src/main/java/com/yk/servlet/ServletInitializer.java
@@ -1,9 +1,14 @@
 package com.yk.servlet;
 
 import com.yk.application.WebsiteInitializer;
+import com.yk.filter.UserFilter;
+import com.yk.listener.SecondListener;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
 
 /**
  * 继承了WebsiteInitializer就会被WebsiteServletContainerInitializer类感知到，然后被初始化
@@ -13,5 +18,10 @@ public class ServletInitializer implements WebsiteInitializer {
     public void onStartup(ServletContext servletContext) {
         ServletRegistration.Dynamic servlet = servletContext.addServlet("userServlet", new IndexServlet());
         servlet.addMapping("/index");
+
+        FilterRegistration.Dynamic filter = servletContext.addFilter("userFilter", new UserFilter());
+        filter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.ASYNC), false, "/*");
+
+//        servletContext.addListener(new SecondListener());
     }
 }
