4c1069aa58b3b89674d1677640535e7531b6754b
aliandpeach <aliandpeach@gmail.com>
2020-08-15 16:44:09 +0800

    commit

--- container_initializer/src/main/java/com/yk/container/WebsiteServletContainerInitializer.java
+++ container_initializer/src/main/java/com/yk/container/WebsiteServletContainerInitializer.java
@@ -17,7 +17,7 @@ import java.util.Set;
  */
 //该类作为实现Servlet3.0的启动类，配置在META-INF/services/javax.servlet.ServletContainerInitializer
 //然后被tomcat实现的spi技术
-// (java 原版的spi技术主要类是ServiceLoader, tomcat重新实现了ServiceLoader类，名字是WebappServiceLader，用法基本一致)所启动
+// (java 原版的spi技术主要类是ServiceLoader, tomcat重新实现了ServiceLoader类，名字是WebappServiceLoader，用法基本一致)所启动
 // 主要过程是：先根据services下的配置文件读取ServletContainerInitializer的实现类，
 // 紧接着扫描@HandlesTypes中配置的接口或者注解，把接口（WebsiteInitializer）实现类的搜集起来,
 // 然后调用onStartUp方法，传入刚才搜集的实现类
